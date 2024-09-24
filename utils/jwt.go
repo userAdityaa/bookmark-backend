1989c83ff291e371591c20bbe8fdc2af68517c1b
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-09-24 14:50:39 +0530

    logic: Added the logic for generating and validating jwt token

---
+++ utils/jwt.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
+type JWTCalims struct {
+	UserID uint   `json:"user_id"`
+	Email  string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uint, email string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &JWTCalims{
+		UserID: userID,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) (*JWTCalims, error) {
+	claims := &JWTCalims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+	return claims, nil
+}
